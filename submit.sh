8ff2cc618bc5cce67abed02951bf8046b0ca5f58
PythonFZ <fzills@icp.uni-stuttgart.de>
2023-08-23 08:01:02 +0200

    train_4

--- submit.sh
+++ submit.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=dgx
+#SBATCH --partition=single
 ##SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
+#SBATCH --mem=16gb
 
-#module load gcc
-#module load openmpi/1.8.3_gcc-11.3
-#module load cp2k/2023.1_ompi-4.1.4
+module load devel/cuda/11.6
 
-#module load cuda
-
-dvc repro
-
+dvc exp run --run-all
